07535921537abe29009c7e9cee7baddd525bd4d0
nurmohammadweb <nurmohammad11266@gmail.com>
2025-09-19 09:05:59 +0600

    first commit of new react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Countries from './componets/countries/countries'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
